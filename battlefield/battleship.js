dcca25b42a25d6d3618c2791c8c2200d81978538
Jerome Hardaway <jerome@vetswhocode.io>
2017-11-30 09:38:26 -0600

    save

--- battlefield/battleship.js
+++ battlefield/battleship.js
@@ -25,3 +25,5 @@ while (isSunk == false) {
     }
   }
 }
+var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accureacy was " + (3/guesses);
+alert(stats);
